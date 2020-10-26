531d514bc83c1f2d977c1f648e1291433fa79bc0
Devon Burriss <devon@chimplab.co>
2020-10-26 19:45:57 +0100

    Adds some helpers for Result

--- src/Hotspot.Library/Result.fs
+++ src/Hotspot.Library/Result.fs
@@ -7,3 +7,56 @@ module Result =
         | (Error e, _) -> Error e
         | (_, Error e) -> Error e
 
+    // from https://github.com/swlaschin/DmmfWorkshop/blob/master/src/E-ModelingErrors/Result.fsx
+    
+    let bimap onSuccess onError xR =
+        match xR with
+        | Ok x -> onSuccess x
+        | Error err -> onError err
+        
+    let iter (f : _ -> unit) result =
+        Result.map f result |> ignore
+        
+[<AutoOpen>]
+module ResultComputationExpression =
+
+    type ResultBuilder() =
+        member __.Return(x) = Ok x
+        member __.Bind(x, f) = Result.bind f x
+
+        member __.ReturnFrom(x) = x
+        member this.Zero() = this.Return ()
+
+        member __.Delay(f) = f
+        member __.Run(f) = f()
+
+        member this.While(guard, body) =
+            if not (guard())
+            then this.Zero()
+            else this.Bind( body(), fun () ->
+                this.While(guard, body))
+
+        member this.TryWith(body, handler) =
+            try this.ReturnFrom(body())
+            with e -> handler e
+
+        member this.TryFinally(body, compensation) =
+            try this.ReturnFrom(body())
+            finally compensation()
+
+        member this.Using(disposable:#System.IDisposable, body) =
+            let body' = fun () -> body disposable
+            this.TryFinally(body', fun () ->
+                match disposable with
+                    | null -> ()
+                    | disp -> disp.Dispose())
+
+        member this.For(sequence:seq<_>, body) =
+            this.Using(sequence.GetEnumerator(),fun enum ->
+                this.While(enum.MoveNext,
+                    this.Delay(fun () -> body enum.Current)))
+
+        member this.Combine (a,b) =
+            this.Bind(a, fun () -> b())
+
+    let result = new ResultBuilder()
